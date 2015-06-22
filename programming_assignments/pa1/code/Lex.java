aa23cf6183d7481ebc2640caad0f38cccb941ee0
Adam-K-P <akpinarb@ucsc.edu>
2015-06-22 16:12:07 -0700

    worked on pa1

---
+++ programming_assignments/pa1/code/Lex.java
@@ -0,0 +1,31 @@
+import static java.lang.System.out;
+import static java.lang.System.err;
+import java.io.*;
+
+class Lex {
+   public static void main(String[] args) {
+      if (args.length != 2) 
+         throw new RuntimeException
+                   ("There must be two command line arguments");
+      readFile(args[0]);
+   }
+   static void readFile(String filename) {
+      try {
+         FileReader file = new FileReader(filename);
+         BufferedReader buffer = new BufferedReader(file);
+         String line;
+         while ((line = buffer.readLine()) != null)
+            out.printf("%s\n", line);
+      }
+      catch (FileNotFoundException ex) {
+         err.printf("File: %s not found\n", filename);
+      }
+      catch (IOException ex) {
+         err.printf("Error reading file: %s\n", filename);
+      }
+   }
+}
+
+
+                                       
+
---
+++ programming_assignments/pa1/code/List.java
@@ -0,0 +1,5 @@
+import static java.lang.System.out;
+import static java.lang.System.err;
+
+class List {
+

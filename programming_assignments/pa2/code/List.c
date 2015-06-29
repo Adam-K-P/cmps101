ce145236e4164645573ade3e30d5576e2f57099c
Adam-K-P <akpinarb@ucsc.edu>
2015-06-29 16:31:01 -0700

    working on pa2

--- programming_assignments/pa2/code/List.c
+++ programming_assignments/pa2/code/List.c
@@ -2,6 +2,7 @@
    akpinarb
    pa2           */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "List.h"
@@ -9,92 +10,100 @@
 typedef struct node {
    struct node *next;
    struct node *prev;
+   int data;
 }node;
 
 typedef struct List {
    node *front;
    node *back;
+   node *cursor;
    int index;
    int length;
 }List;
 
+static void error(char *function, char *message) {
+   printf("Error in - %s: %s\n", function, message);
+   exit(EXIT_FAILURE);
+}
+
 // newList
 /* List constructor */
 List newList(void) {
-   return NULL;
+   List *thisList = malloc(sizeof(List))
+   clear(thisList);
+   return thisList;
 }
 
 // freeList
 /* List destructor */
 void freeList(List *pL){
-   (void)pL;
+   for (node *curr = pL->front; curr != NULL;) {
+       node *temp = curr;
+       curr = curr->next;
+       free(temp); 
+       *temp = NULL;
+   }
+   free(pL);
+   *pL = NULL;
 }
 
 // length
 /* Get length */
-int length(List L){
-   (void)L;
-   return 0;
-}
+int length(List L){ return L.length; }
 
 // index
 /* Get index */
-int index(List L) {
-   (void)L;
-   return 0;
-}
+int index(List L) { return L.index; }
 
 // front
 /* Get front element */
-int front(List L) {
-   (void)L;
-   return 0;
+int front(List L) { 
+   if (length == 0) error("front", "length is 0");
+   return L.front->data; 
 }
 
 // back
 /* Get back element */
-int back(List L) {
-   (void)L;
-   return 0;
-}
+int back(List L) { return L.back->data; }
 
 // get
 /* Get element at cursor */
-int back(List L) {
-   (void)L;
-   return 0;
-}
+int get(List L) { return L.cursor->data; }
 
 // equals
 /* Find if List A equals List B */
-int equals(List A, List B) {
-   (void)A;
-   (void)B;
-   return 0;
+int equals(List A, List B) {  
+   if ( ((A == NULL) ^ (B == NULL)) || 
+        ((A.length == 0) ^ (B.length == 0)) ) return false;
+   for (node *Anode, node*Bnode; 
+        Anode != NULL && Bnode != NULL;
+        Anode = Anode.next, Bnode = Bnode.next ) 
+      { if (Anode != Bnode) return false; }
+   return true;
 }
 
 // clear
 /* Clear out a List to defaults */
-void clear(List L) {
-   (void) L;
+void clear(List L) { 
+   thisList.front  = NULL;
+   thisList.back   = NULL;
+   thisList.cursor = NULL;
+   thisList.index  = -1;
+   thisList.length = 0;
 }
 
 // moveFront
 /* Move cursor to front node */
-void moveFront(List L) {
-   (void)L;
-}
+void moveFront(List L) { cursor = front; }
 
 // moveBack
 /* Move cursor to back node */
-void moveBack(List L) {
-   (void)L;
-}
+void moveBack(List L) { cursor = back; }
 
 // movePrev
 /* Move cursor to previous node */
-void movePrev(List L) {
-   (void)L;
+void movePrev(List L) { 
+
 }
 
 // moveNext
